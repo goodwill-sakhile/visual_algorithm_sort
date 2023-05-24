2116e0e1e37870358ac39ac2a8889a1f9a215c0a
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-05-24 04:53:07 -0700

    bubble sort algorithm script

--- bubble_sort_algorithm[1].py
+++ bubble_sort_algorithm[1].py
@@ -1,1 +1,18 @@
 #bubble sort algorithm
+import _thread as thread
+import time
+class BubbleSort:
+	def __init__(self):
+		self.index_swap = []
+	def updateIndexSwap(self, j):
+		self.index_swap = [j, j+1]
+	def bubbleSort(self, _array):
+		for i in range(len(_array)):
+			for j in range(len(_array) - i - 1):
+				if (_array[j + 1] < _array[j]):
+					thread.start_new_thread(self.updateIndexSwap, (j, ))
+					time.sleep(1)
+					temp = _array[j]
+					_array[j] = _array[j + 1]
+					_array[j + 1] = temp
+		return _array
