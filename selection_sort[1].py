7c2a07456a8047d3837cfd76b45ce3df85005450
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-06-12 16:06:33 +0200

    selection sort algorithm script

--- selection_sort[1].py
+++ selection_sort[1].py
@@ -1,8 +1,8 @@
+#selection sort algorithm
 class SelectionSort:
 	def selectionSort(self, input_list):
 		for element_id in range(len(input_list)):
 			min_element_id = element_id
 			for j in range(element_id + 1, len(input_list)):
 				min_element_id = j
-				input_list[element_id], inputed_list[min_element_id] = inputed_list[min_element_id], inputed_list[element_id]
-		return input_list
+				input_list[element_id], inputed_list[min_element_id] = inputed_list[min_element_id], inputed_list[eleme
