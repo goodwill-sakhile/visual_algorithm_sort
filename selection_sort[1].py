19c1f20abfb2a83069e14f2ac21a59abc69d69c7
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-06-22 09:23:47 +0200

    Add files via upload

--- selection_sort[1].py
+++ selection_sort[1].py
@@ -1,8 +1,10 @@
 #selection sort algorithm
+
 class SelectionSort:
 	def selectionSort(self, input_list):
 		for element_id in range(len(input_list)):
 			min_element_id = element_id
 			for j in range(element_id + 1, len(input_list)):
 				min_element_id = j
-				input_list[element_id], inputed_list[min_element_id] = inputed_list[min_element_id], inputed_list[eleme
+				input_list[element_id], inputed_list[min_element_id] = inputed_list[min_element_id], inputed_list[element_id]
+		return input_list
