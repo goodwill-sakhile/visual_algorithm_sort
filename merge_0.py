ebff92f4daab59a847eabdbf0cfd1bc288681904
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-10-02 15:17:38 +0200

    merge sort algorithm

--- merge_0.py
+++ merge_0.py
@@ -1,33 +1,42 @@
-def mergeSort(unsorted_list):
-    if len(unsorted_list)  <= 1:
-        return unsorted_list
-    middle = len(unsorted_list) // 2
-    left_list = unsorted_list[:middle]
-    right_list = unsorted_list[middle:]
-    #print(right_list)
-    left_list = mergeSort(left_list)
-    right_list = mergeSort(right_list)
+class MergeSort:
+    def __init__(self, **kwrags):
+        self.swap_list = []
+    def mergeSort(self, unsorted_list):
+        if len(unsorted_list)  <= 1:
+            return unsorted_list
+        middle = len(unsorted_list) // 2
+        left_list = unsorted_list[:middle]
+        right_list = unsorted_list[middle:]
+        #print(right_list)
+        left_list = self.mergeSort(left_list)
+        right_list = self.mergeSort(right_list)
     
-    return list(merge(left_list, right_list)[1])
-def merge(left_half, right_half):
-    res = []
-    swap_list = []
-    while len(left_half) != 0 and len(right_half) != 0:
-        count = 0
-        if left_half[0][0] < right_half[0][0]:
-            res.append(left_half[0])
-            swap_list.append([left_half[0][0], right_half[0][0]])
-            print([left_half[0][0], right_half[0][0]])
-            left_half.remove(left_half[0])
+        x, y =  list(self.merge(left_list, right_list))
+        #print("y", y)
+        print("self.swap", self.swap_list)
+        return x
+    def merge(self, left_half, right_half):
+        res = []
+        swap_list = []
+        while len(left_half) != 0 and len(right_half) != 0:
+            count = 0
+            if left_half[0][0] < right_half[0][0]:
+                res.append(left_half[0])
+                self.swap_list.append([left_half[0][1], right_half[0][1]])
+                #print([left_half[0][1], right_half[0][1]])
+                left_half.remove(left_half[0])
+            else:
+                self.swap_list.append([right_half[0][1], left_half[0][1]])
+                #print([right_half[0][1], left_half[0][1]])
+                res.append(right_half[0])
+                right_half.remove(right_half[0])
+        if len(left_half) == 0:
+            res = res + right_half
         else:
-            swap_list.append([right_half[0][0], left_half[0][0]])
-            print([right_half[0][0], left_half[0][0]])
-            res.append(right_half[0])
-            right_half.remove(right_half[0])
-    if len(left_half) == 0:
-        res = res + right_half
-    else:
-        res = res + left_half
-    return swap_list, res
+            res = res + left_half
+        return res, swap_list
 unsorted_list = [[64, 0], [34, 1], [25, 2], [12, 3], [22, 4], [11, 5], [90, 6]]
-#print(mergeSort(unsorted_list))
+merge = MergeSort()
+
+print(merge.mergeSort(unsorted_list))
+#print("swap_swap", merge.swap_list)
