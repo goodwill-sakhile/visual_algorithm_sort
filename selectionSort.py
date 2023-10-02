e17e4e0d05caa1464696f98e4e5f9be37558d962
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-10-02 15:18:22 +0200

    selection sort algorithm

--- selectionSort.py
+++ selectionSort.py
@@ -1,10 +1,12 @@
 def selectionSort(input_list):
+    swap_list = []
     for i in range(len(input_list)):
         min_i = i
         for j in range(i + 1, len(input_list)):
-            if input_list[min_i] > input_list [j]:
+            if input_list[min_i][0] > input_list [j][0]:
                 min_i = j
-        input_list[i], input_list[min_i] = input_list[min_i], input_list[i]
-_list = [19, 2, 31, 45, 30, 11, 121, 27]
+        input_list[i][0], input_list[min_i][0] = input_list[min_i][0], input_list[i][0]
+        swap_list.append([i, min_i])
+_list = [[19, 0], [2, 1], [31, 2], [45, 3], [30, 4], [11, 5], [121, 6], [27, 7]]
 selectionSort(_list)
 print(_list)
