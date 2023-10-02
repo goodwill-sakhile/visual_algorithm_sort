6c4aeacdeda1597841c30aed51a210ba3c20bb4e
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-10-02 15:16:22 +0200

    insertion sort script

--- insertionSort.py
+++ insertionSort.py
@@ -1,11 +1,14 @@
 def insertionSort(input_list):
+    swap_list = []
     for i in range(1, len(input_list)):
         j =i - 1
-        next_element = input_list[i]
-        while (input_list[j] > next_element) and (j >= 0):
-            input_list[j+1] = input_list[j]
+        next_element = input_list[i][0]
+        while (input_list[j][0] > next_element) and (j >= 0):
+            input_list[j+1][0] = input_list[j][0]
+            swap_list.append([i, j])
             j = j - 1
-        input_list[j+1] = next_element
-list = [19, 2, 31, 43, 30, 11, 121, 27]
+        input_list[j+1][0] = next_element
+    print(swap_list)
+list = [[19, 0], [2, 1], [31, 2], [43, 3], [30, 4], [11, 5], [121, 6], [27, 7]]
 insertionSort(list)
 print(list)
