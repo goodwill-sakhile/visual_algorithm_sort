95197d972f408f3303228fc12543bd54de89fcd9
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-10-04 08:53:51 +0200

    merge sort algorithm script

--- merge_0.py
+++ merge_0.py
@@ -1,6 +1,22 @@
+import random
 class MergeSort:
     def __init__(self, **kwrags):
         self.swap_list = []
+    def putIndexes(self, pool):
+	    #print("pool", pool)
+	    _pool = []
+	    for i in range(len(pool)):
+	        _pool.append([pool[i], i])
+	    #print("POOL", _pool)
+	    return _pool
+    def generateValues(self, _pool):
+        new_pool = []
+        #generate values randomly that will be sorted
+        for i in range(len(_pool)):
+            value  = random.choice(_pool)
+            _pool.remove(value)
+            new_pool.append(value)
+        return new_pool
     def mergeSort(self, unsorted_list):
         if len(unsorted_list)  <= 1:
             return unsorted_list
@@ -13,7 +29,7 @@ class MergeSort:
     
         x, y =  list(self.merge(left_list, right_list))
         #print("y", y)
-        print("self.swap", self.swap_list)
+        #print("self.swap", self.swap_list)
         return x
     def merge(self, left_half, right_half):
         res = []
@@ -22,11 +38,11 @@ class MergeSort:
             count = 0
             if left_half[0][0] < right_half[0][0]:
                 res.append(left_half[0])
-                self.swap_list.append([left_half[0][1], right_half[0][1]])
+                #self.swap_list.append([right_half[0][1], left_half[0][1]])
                 #print([left_half[0][1], right_half[0][1]])
                 left_half.remove(left_half[0])
             else:
-                self.swap_list.append([right_half[0][1], left_half[0][1]])
+                self.swap_list.append([left_half[0][1], right_half[0][1]])
                 #print([right_half[0][1], left_half[0][1]])
                 res.append(right_half[0])
                 right_half.remove(right_half[0])
@@ -35,8 +51,14 @@ class MergeSort:
         else:
             res = res + left_half
         return res, swap_list
-unsorted_list = [[64, 0], [34, 1], [25, 2], [12, 3], [22, 4], [11, 5], [90, 6]]
+#unsorted_list = [[64, 0], [34, 1], [25, 2], [12, 3], [22, 4], [11, 5], [90, 6]]
 merge = MergeSort()
-
-print(merge.mergeSort(unsorted_list))
+pool = merge.generateValues(list(range(100)))
+#print(pool)
+_list = merge.putIndexes(pool)
+#print("index_included:", _list)
+#print("")
+#print("Sorted:", merge.mergeSort(_list))
+#print("")
+#print("Swap:", merge.swap_list)
 #print("swap_swap", merge.swap_list)
