cc2823b41aaa9402664c10ad20f97ae72defa8eb
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-10-09 10:41:53 +0200

    merge sort algorithm script

--- merge_sort.py
+++ merge_sort.py
@@ -4,15 +4,18 @@ def mergeSort(unsorted_list):
     middle = len(unsorted_list) // 2
     left_list = unsorted_list[:middle]
     right_list = unsorted_list[middle:]
+    #print(right_list)
     left_list = mergeSort(left_list)
     right_list = mergeSort(right_list)
+    
     return list(merge(left_list, right_list))
 def merge(left_half, right_half):
     res = []
     while len(left_half) != 0 and len(right_half) != 0:
         if left_half[0] < right_half[0]:
             res.append(left_half[0])
-            lef_half.remove(left_half[0])
+            print(left_half[0], right_half[0])
+            left_half.remove(left_half[0])
         else:
             res.append(right_half[0])
             right_half.remove(right_half[0])
