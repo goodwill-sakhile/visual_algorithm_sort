bc18aa4592c3b3a57fa04e6e31296803c752a3c2
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-10-09 10:39:41 +0200

    merge sort algorithm script

--- merge_0.py
+++ merge_0.py
@@ -23,15 +23,11 @@ class MergeSort:
         middle = len(unsorted_list) // 2
         left_list = unsorted_list[:middle]
         right_list = unsorted_list[middle:]
-        #print(right_list)
         counter+=1
         print("counter:", counter)
         left_list = self.mergeSort(left_list, counter)
         right_list = self.mergeSort(right_list, middle)
-    
-        x, y =  list(self.merge(left_list, right_list))
-        #print("y", y)
-        #print("self.swap", self.swap_list)
+        x =  list(self.merge(left_list, right_list))
         return x
     def merge(self, left_half, right_half):
         res = []
@@ -41,12 +37,9 @@ class MergeSort:
             
             if left_half[0][0] < right_half[0][0]:
                 res.append(left_half[0])
-                #self.swap_list.append([right_half[0][1], left_half[0][1]])
-                #print([left_half[0][1], right_half[0][1]])
                 left_half.remove(left_half[0])
             else:
-                self.swap_list.append([left_half[0][1], right_half[0][1]])
-                #print([right_half[0][1], left_half[0][1]])
+                #self.swap_list.append([left_half[0][1], right_half[0][1]])
                 res.append(right_half[0])
                 right_half.remove(right_half[0])
             
@@ -54,7 +47,7 @@ class MergeSort:
             res = res + right_half
         else:
             res = res + left_half
-        return res, swap_list
+        return res
 #unsorted_list = [[64, 0], [34, 1], [25, 2], [12, 3], [22, 4], [11, 5], [90, 6]]
 merge = MergeSort()
 pool = merge.generateValues(list(range(100)))
