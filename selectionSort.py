b83be7424bf2d10af12e18fea77803e7b53e8319
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-10-11 11:22:10 +0200

    selection sort script

--- selectionSort.py
+++ selectionSort.py
@@ -1,12 +1,11 @@
-def selectionSort(input_list):
-    swap_list = []
-    for i in range(len(input_list)):
-        min_i = i
-        for j in range(i + 1, len(input_list)):
-            if input_list[min_i][0] > input_list [j][0]:
-                min_i = j
-        input_list[i][0], input_list[min_i][0] = input_list[min_i][0], input_list[i][0]
-        swap_list.append([i, min_i])
-_list = [[19, 0], [2, 1], [31, 2], [45, 3], [30, 4], [11, 5], [121, 6], [27, 7]]
-selectionSort(_list)
-print(_list)
+class SelectionSort:    
+    def selectionSort(self, input_list):
+        swap_list = []
+        for i in range(len(input_list)):
+            min_i = i
+            for j in range(i + 1, len(input_list)):
+                if input_list[min_i][0] > input_list [j][0]:
+                    min_i = j
+            input_list[i][0], input_list[min_i][0] = input_list[min_i][0], input_list[i][0]
+            swap_list.append([i, min_i])
+        return swap_list
